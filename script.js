63fe68cb59fc50d4afe0b7179130503b3aa50fd6
rrifky <207006052@student.unsil.ac.id>
2023-02-18 19:45:49 +0700

    initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+alert('Halooo :D');
+var ulang = true;
+
+while (ulang === true) {
+  var nama = prompt('Siapa nama kamu?');
+  alert('Selamat ' + nama + ' kamu adalah anak TOLOL');
+  ulang = confirm('Ulang?');
+}
+
+alert('Sip');
